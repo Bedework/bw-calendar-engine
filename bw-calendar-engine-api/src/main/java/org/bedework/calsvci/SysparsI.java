550df442415b46071719505a38880936d2cd50a4
douglm <douglm@rpi.edu>
2013-05-19 23:44:28 -0400

    Missed update to sysparsi

--- bw-calendar-engine-api/src/main/java/org/bedework/calsvci/SysparsI.java
+++ bw-calendar-engine-api/src/main/java/org/bedework/calsvci/SysparsI.java
@@ -32,12 +32,6 @@ import java.util.Locale;
  *
  */
 public interface SysparsI extends Serializable {
-  /** Retrieve systemName supplied at init
-   *
-   * @return String name
-   */
-  public String getSystemName();
-
   /** Get the (possibly cached) system pars using name supplied at init
    *
    * @return BwSystem object
@@ -61,18 +55,6 @@ public interface SysparsI extends Serializable {
    */
   public void update(BwSystem val) throws CalFacadeException;
 
-  /** Get a name uniquely.identifying this system. This should take the form <br/>
-   *   name@host
-   * <br/>where<ul>
-   * <li>name identifies the particular calendar system at the site</li>
-   * <li>host part identifies the domain of the site.</li>..
-   * </ul>
-   *
-   * @return String    globally unique system identifier.
-   * @throws CalFacadeException
-   */
-  public String getSysid() throws CalFacadeException;
-
   /** Get the locales supported by bedework.
    *
    * @return Collection of locale
@@ -80,40 +62,12 @@ public interface SysparsI extends Serializable {
    */
   public Collection<Locale> getSupportedLocales() throws CalFacadeException;
 
-  /** Add a locale to the supported locales list.
-   *
-   * @param loc
-   * @throws CalFacadeException
-   */
-  public void addLocale(Locale loc) throws CalFacadeException;
-
-  /** Remove a locale from the supported locales list.
-   *
-   * @param loc
-   * @throws CalFacadeException
-   */
-  public void removeLocale(Locale loc) throws CalFacadeException;
-
   /** Get the list of root accounts.
    *
    * @return Collection of String
    * @throws CalFacadeException
    */
   public Collection<String> getRootUsers() throws CalFacadeException;
-
-  /** Add an account to the list of root accounts.
-   *
-   * @param val
-   * @throws CalFacadeException
-   */
-  public void addRootUser(String val) throws CalFacadeException;
-
-  /** Remove an account from the list of root accounts.
-   *
-   * @param val
-   * @throws CalFacadeException
-   */
-  public void removeRootUser(String val) throws CalFacadeException;
 
   /** See if this is a calendar super user
    *
