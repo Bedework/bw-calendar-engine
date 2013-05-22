c0f4d25c3a2c1c75fb8f491df2d0b8dc410e089c
douglm <douglm@rpi.edu>
2013-05-21 23:16:05 -0400

    Add CalAddrPrefixes to BasicSystemProperties

--- bw-calendar-engine-facade/src/main/java/org/bedework/calfacade/configs/BasicSystemProperties.java
+++ bw-calendar-engine-facade/src/main/java/org/bedework/calfacade/configs/BasicSystemProperties.java
@@ -37,227 +37,255 @@ public interface BasicSystemProperties extends Serializable {
    *
    * @param val    String
    */
-  public void setPrincipalRoot(final String val);
+  void setPrincipalRoot(String val);
 
   /** get the principal root e.g. "/principals"
    *
    * @return String
    */
-  public String getPrincipalRoot();
+  String getPrincipalRoot();
 
   /** Set the user principal root e.g. "/principals/users"
    *
    * @param val    String
    */
-  public void setUserPrincipalRoot(final String val);
+  void setUserPrincipalRoot(String val);
 
   /** get the principal root e.g. "/principals/users"
    *
    * @return String
    */
-  public String getUserPrincipalRoot();
+  String getUserPrincipalRoot();
 
   /** Set the group principal root e.g. "/principals/groups"
    *
    * @param val    String
    */
-  public void setGroupPrincipalRoot(final String val);
+  void setGroupPrincipalRoot(String val);
 
   /** get the group principal root e.g. "/principals/groups"
    *
    * @return String
    */
-  public String getGroupPrincipalRoot();
+  String getGroupPrincipalRoot();
 
   /** Set the bedework admin group principal root
    * e.g. "/principals/groups/bwadmin"
    *
    * @param val    String
    */
-  public void setBwadmingroupPrincipalRoot(final String val);
+  void setBwadmingroupPrincipalRoot(String val);
 
   /** Get the bedework admin group principal root
    * e.g. "/principals/groups/bwadmin"
    *
    * @return String
    */
-  public String getBwadmingroupPrincipalRoot();
+  String getBwadmingroupPrincipalRoot();
 
   /** Set the resource principal root e.g. "/principals/resources"
    *
    * @param val    String
    */
-  public void setResourcePrincipalRoot(final String val);
+  void setResourcePrincipalRoot(String val);
 
   /** get the resource principal root e.g. "/principals/resources"
    *
    * @return String
    */
-  public String getResourcePrincipalRoot();
+  String getResourcePrincipalRoot();
 
   /** Set the venue principal root e.g. "/principals/locations"
    *
    * @param val    String
    */
-  public void setVenuePrincipalRoot(final String val);
+  void setVenuePrincipalRoot(String val);
 
   /** get the venue principal root e.g. "/principals/locations"
    *
    * @return String
    */
-  public String getVenuePrincipalRoot();
+  String getVenuePrincipalRoot();
 
   /** Set the ticket principal root e.g. "/principals/tickets"
    *
    * @param val    String
    */
-  public void setTicketPrincipalRoot(final String val);
+  void setTicketPrincipalRoot(String val);
 
   /** get the ticket principal root e.g. "/principals/tickets"
    *
    * @return String
    */
-  public String getTicketPrincipalRoot();
+  String getTicketPrincipalRoot();
 
   /** Set the host principal root e.g. "/principals/hosts"
    *
    * @param val    String
    */
-  public void setHostPrincipalRoot(final String val);
+  void setHostPrincipalRoot(String val);
 
   /** get the host principal root e.g. "/principals/hosts"
    *
    * @return String
    */
-  public String getHostPrincipalRoot();
+  String getHostPrincipalRoot();
 
   /** Set the public Calendar Root
    *
    * @param val    String
    */
-  public void setPublicCalendarRoot(final String val);
+  void setPublicCalendarRoot(String val);
 
   /** Get the publicCalendarRoot
    *
    * @return String   publicCalendarRoot
    */
   @MBeanInfo("public Calendar Root - do not change")
-  public String getPublicCalendarRoot();
+  String getPublicCalendarRoot();
 
   /** Set the user Calendar Root
    *
    * @param val    String
    */
-  public void setUserCalendarRoot(final String val);
+  void setUserCalendarRoot(String val);
 
   /** Get the userCalendarRoot
    *
    * @return String   userCalendarRoot
    */
   @MBeanInfo("user Calendar Root - do not change")
-  public String getUserCalendarRoot();
+  String getUserCalendarRoot();
 
   /** Set the user default calendar
    *
    * @param val    String
    */
-  public void setUserDefaultCalendar(final String val);
+  void setUserDefaultCalendar(String val);
 
   /** Get the userDefaultCalendar
    *
    * @return String   userDefaultCalendar
    */
   @MBeanInfo("userDefaultCalendar - do not change")
-  public String getUserDefaultCalendar();
+  String getUserDefaultCalendar();
 
   /** Set the defaultNotificationsName
    *
    * @param val
    */
-  public void setDefaultNotificationsName(final String val);
+  void setDefaultNotificationsName(String val);
 
   /** Get the defaultNotificationsName
    *
    * @return flag
    */
   @MBeanInfo("name of notifications collection - do not change")
-  public String getDefaultNotificationsName();
+  String getDefaultNotificationsName();
 
   /** Set the defaultReferencesName
    *
    * @param val
    */
-  public void setDefaultReferencesName(final String val);
+  void setDefaultReferencesName(String val);
 
   /** Get the defaultReferencesName
    *
    * @return flag
    */
   @MBeanInfo("name of default references collection - do not change")
-  public String getDefaultReferencesName();
+  String getDefaultReferencesName();
 
   /** Set the user inbox name
    *
    * @param val    String
    */
-  public void setUserInbox(final String val);
+  void setUserInbox(String val);
 
   /** Get the user inbox name
    *
    * @return String   user inbox
    */
   @MBeanInfo("user inbox name - do not change")
-  public String getUserInbox();
+  String getUserInbox();
 
   /** Set the user outbox
    *
    * @param val    String
    */
-  public void setUserOutbox(final String val);
+  void setUserOutbox(String val);
 
   /** Get the user outbox
    *
    * @return String   user outbox
    */
   @MBeanInfo("user outbox - do not change")
-  public String getUserOutbox();
+  String getUserOutbox();
 
   /** Set the public user
    *
    * @param val    String
    */
-  public void setPublicUser(final String val);
+  void setPublicUser(String val);
 
   /**
    *
    * @return String
    */
   @MBeanInfo("Account name for public entities - one not in the directory")
-  public String getPublicUser();
+  String getPublicUser();
 
   /** Set the path to the root for indexes
    *
    * @param val    String index root
    */
-  public void setIndexRoot(final String val);
+  void setIndexRoot(String val);
 
   /** Get the path to the root for indexes
    *
    * @return String
    */
   @MBeanInfo("path to the root for non-solr indexes")
-  public String getIndexRoot();
+  String getIndexRoot();
 
   /** Set the global resources path
    *
    * @param val
    */
-  public void setGlobalResourcesPath(final String val);
+  void setGlobalResourcesPath(String val);
 
   /** Get the global resources path
    *
    * @return token
    */
   @MBeanInfo("The global resources path")
-  public String getGlobalResourcesPath();
+  String getGlobalResourcesPath();
+
+  /** To enable mapping of calendar addresses e.g. mailto:fred@example.org
+   *  on to principals we need to either do a directory lookup or have
+   *  some sort of pattern map.
+   *
+   * <p>Setting a caladdr prefix enables pattern mapping. By default
+   * calendar addresses are users
+   *
+   * <pre>
+   * &lt;caladdrPrefixes classname="org.bedework.calfacade.configs.CalAddrPrefixes">
+   *   &lt;!--
+   *        This would specify that any calendar user address starting with loc_
+   *        is to be treated as a location, e.g. we might get
+   *            mailto:loc_vcc315@example.org
+   *      &lt;location>loc_&lt;/location>
+   *       -->
+   *    &lt;/caladdrPrefixes>
+   * </pre>
+   *
+   * @param val
+   */
+  void setCalAddrPrefixes(CalAddrPrefixes val);
+
+  /**
+   * @return CalAddrPrefixes or null
+   */
+  @MBeanInfo("Calendar address prefixes")
+  CalAddrPrefixes getCalAddrPrefixes();
 }
